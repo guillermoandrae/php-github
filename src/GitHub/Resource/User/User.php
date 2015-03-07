d2aeb59c6c536546503259bd699d9c3e6463a9e0
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-03-07 14:09:20 -0500

    Fleshed out the user resource.

--- src/GitHub/Resource/User/User.php
+++ src/GitHub/Resource/User/User.php
@@ -15,37 +15,106 @@ class User extends ResourceAbstract
 {
     use LoginAwareTrait;
 
+    /**
+     * @var string
+     */
+    protected $gistsUrl;
+
+    /**
+     * @var string
+     */
+    protected $starredUrl;
+
+    /**
+     * @var string
+     */
+    protected $subscriptionsUrl;
+
+    /**
+     * @var string
+     */
+    protected $organizationsUrl;
+
+    /**
+     * @var string
+     */
+    protected $reposUrl;
+
+    /**
+     * @var string
+     */
+    protected $eventsUrl;
+
+    /**
+     * @var string
+     */
+    protected $receivedEventsUrl;
+
+    /**
+     * @var boolean
+     */
+    protected $siteAdmin;
+
+    /**
+     * @var boolean
+     */
+    protected $hireable;
+
+    /**
+     * @var string
+     */
+    protected $bio;
+
+    /**
+     * @var string
+     */
+    protected $updatedAt;
+
+    /**
+     * @var string
+     */
+    protected $followersUrl;
+
+    /**
+     * @var string
+     */
+    protected $followingUrl;
+
+    /**
+     * @return bool
+     */
     public function isHireable()
     {
         return $this->hireable;
     }
 
-    public function getFollowersUrl()
-    {
-        return $this->followersUrl;
-    }
-
-    public function getFollowingUrl()
-    {
-        return $this->followingUrl;
-    }
-
+    /**
+     * @return bool
+     */
     public function isSiteAdmin()
     {
         return $this->siteAdmin;
     }
 
-
+    /**
+     * @return string
+     */
     public function getSubscriptionsUrl()
     {
         return $this->subscriptionsUrl;
     }
 
+    /**
+     * @return string
+     */
     public function getOrganizationsUrl()
     {
         return $this->organizationsUrl;
     }
 
+    /**
+     * @return string
+     */
     public function getBio()
     {
         return $this->bio;
@@ -57,11 +126,64 @@ class User extends ResourceAbstract
      * @param boolean OPTIONAL Whether or not the method should return the datetime string
      * @return \DateTime
      */
-    public function getUpdateddAt($raw = false)
+    public function getUpdatedAt($raw = false)
     {
-        if ($raw) {
-            return $this->updatedAt;
-        }
-        return new \DateTime($this->updatedAt);
+        return $this->getDateTime($this->updatedAt, $raw);
+    }
+
+    /**
+     * @return string
+     */
+    public function getGistsUrl()
+    {
+        return $this->gistsUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStarredUrl()
+    {
+        return $this->starredUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReposUrl()
+    {
+        return $this->reposUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventsUrl()
+    {
+        return $this->eventsUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReceivedEventsUrl()
+    {
+        return $this->receivedEventsUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFollowersUrl()
+    {
+        return $this->followersUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFollowingUrl()
+    {
+        return $this->followingUrl;
     }
 }
--- tests/GitHubTest/Resource/User/UserTest.php
+++ tests/GitHubTest/Resource/User/UserTest.php
@@ -13,11 +13,71 @@ use GitHubTest\TestCase\TestCase;
 
 class UserTest extends TestCase
 {
+    /**
+     * @var \GitHub\Resource\User\User
+     */
     private $user;
 
     public function testGetBio()
     {
         $this->assertSame('There once was...', $this->getUser()->getBio());
+    }
+
+    public function testGetFollowersUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/followers', $this->getUser()->getFollowersUrl());
+    }
+
+    public function testGetFollowingUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/following{/other_user}', $this->getUser()->getFollowingUrl());
+    }
+
+    public function testGetGistsUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/gists{/gist_id}', $this->getUser()->getGistsUrl());
+    }
+
+    public function tesGetStarredUrl()
+    {
+        $this->assertSame('There once was...', $this->getUser()->getStarredUrl());
+    }
+
+    public function testGetSubscriptionsUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/subscriptions', $this->getUser()->getSubscriptionsUrl());
+    }
+
+    public function testGetOrganizationsUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/orgs', $this->getUser()->getOrganizationsUrl());
+    }
+
+    public function testGetReposUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/repos', $this->getUser()->getReposUrl());
+    }
+
+    public function testGetEventsUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/events{/privacy}', $this->getUser()->getEventsUrl());
+    }
+
+    public function testGetReceivedEventsUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/received_events', $this->getUser()->getReceivedEventsUrl());
+    }
+
+    public function testGetStarredUrl()
+    {
+        $this->assertSame('https://api.github.com/users/octocat/starred{/owner}{/repo}', $this->getUser()->getStarredUrl());
+    }
+
+    public function testGetUpdatedAt()
+    {
+        $expectedDateTimeString = '2008-01-14T04:33:35Z';
+        $this->assertEquals($expectedDateTimeString, $this->getUser()->getUpdatedAt(true));
+        $this->assertEquals(new \DateTime($expectedDateTimeString), $this->getUser()->getUpdatedAt());
     }
 
     public function testIsHireable()
@@ -38,6 +98,9 @@ class UserTest extends TestCase
         $this->user = $mapper->find('octocat');
     }
 
+    /**
+     * @return \GitHub\Resource\User\User
+     */
     private function getUser()
     {
         return $this->user;
